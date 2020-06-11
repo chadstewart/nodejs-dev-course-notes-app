dd7a34a8accb0ac837053a2e44d3a84dc99fa0f3
Chad Stewart <chadrhonanstewart@gmail.com>
2020-06-11 07:59:54 -0500

    Completed Section 3 of Complete Node.js Developer Course. Did not install nodemon globally as yet because I rather run the most up to date version. Will just install for each project for now.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
+//const validator = require('validator');
 
 const msg = getNotes();
 
 console.log(msg);
+
+/* console.log(validator.isEmail('chadrs@example.com'));
+console.log(validator.isURL('httr:/example.com')); */
+
+console.log(chalk.green.inverse.bold('Success!!'));
